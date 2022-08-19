912916258dd6cea6da20e5745d73acb40617eab7
guilherme <guilhermeadr23@gmail.com>
2022-08-19 16:21:04 -0300

    criamos o endpointes

--- app.js
+++ app.js
@@ -1,11 +1,84 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
 app.use("/listausuarios",(req,res,next)=>{
-    console.log("passei aqui")
+    
     res.status(200).send({
-        mensagem:"deu certo!!!",
+        mensagem:"aqui é lista de usuários",
         nome: "Maria Eduarda"
+    })
+
+});
+app.use("/listapatrimonio", (req,res,next)=>{
+
+    res.status(200).send({
+    mensagem:"aqui é lista de patrimonios!!!",
+    nome:"Maria Eduarda"
+    })
+
+});
+
+app.use("/somar",(req,res,next)=>{
+    let a = 2;
+    let b = 5;
+    let total = 0;
+    total = a + b;
+    console.log(total);
+    res.status(200).send({
+        resultado: total
+    })
+});
+     
+app.use("/resultado",(req,res,next)=>{
+    const a=8;
+    const b=5;
+    const c=8;
+    const d=10;
+    let media=0;
+    let situacao="";
+    let total=0;
+    total=a+b+c+d;
+    media=total/4;
+    if(media<7){
+       situacao="Reprovado";
+    }else{
+       situacao="Aprovado";
+    }
+    console.log(total);
+
+       res.status(200).send(
+
+          
+           
+           {
+               nome:"Maria Eduarda",
+               nota1:a,
+               nota2:b,
+               nota3:c,
+               nota4:d,
+               resltado:total,
+               media:media,
+               situacao:situacao
+
+
+           }
+       )
+
+});
+
+app.use((req,res,next)=>{
+    const erro = new Error("Não encontrado!");
+    erro.status(404);
+    next(erro);
+
+});
+
+app.use((error,req,res,next)=>{
+    res.status(error.status || 500);
+    return res.json({
+        erro:{
+            mensagem:error.mensagem
+        }
     })
 
 })
